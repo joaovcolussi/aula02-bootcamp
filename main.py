546b83597a35868160e0b1545e109eeb1489869f
João Victor Colussi <victorcolussi@outlook.com>
2025-01-15 09:32:46 -0300

    Feito alguns exercicios

---
+++ main.py
@@ -0,0 +1,56 @@
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# def somar(n1,n2):
+#   return n1+n2
+# inserirNumero1 = int(input("Digite o primeiro número \n"))
+# inserirNumero2 = int(input("Digite o segundo número \n"))
+# print(f"O resultado é : {somar(inserirNumero1,inserirNumero2)} ")
+
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# def restoDivisao(numero):
+#   return numero / 5
+# inserirNumero = float(input("Digite o número: "))
+# print(f"O resto do número que digitou :{restoDivisao(inserirNumero)}")
+
+
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# def multiplicacao(numero1, numero2):
+#   return numero1 * numero2
+# inserirNumero1 = float(input("Digite o primeiro número\n"))
+# inserirNumero2 = float(input("Digite o segundo número\n"))
+# print(f"A multiplicação de {inserirNumero1} * {inserirNumero2} é {multiplicacao(inserirNumero1,inserirNumero2)}")
+
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# def divisaoNumeros(numero1,numero2):
+#   return numero1 / numero2
+# inserirNumero1 = int(input("Digite o primeiro número: \n"))
+# inserirNumero2 = int(input("Digite o segundo número: \n"))
+# print(f"A divisão do número {inserirNumero1} / {inserirNumero2} é {divisaoNumeros(inserirNumero1,inserirNumero2)}")
+
+# Escreva um programa que receba dois números flutuantes e realize sua adição.
+# def somaAdicao(numero1,numero2):
+#   return numero1 +numero2
+# num1 = float(input("Digite o primeiro número: \n"))
+# num2 = float(input("Digite o segundo número: \n"))
+# print(f"A soma de {num1} + {num2} é {somaAdicao(num1,num2)}")
+
+# Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# def numeroMedia(n1,n2):
+#   return (n1+ n2) / 2
+# inserirNumero1 = float(input("Digite o primeiro número: \n"))
+# inserirNumero2 = float(input("Digite o segundo número: \n"))
+# print(f"A média das notas {inserirNumero1} + {inserirNumero2} é {numeroMedia(inserirNumero1,inserirNumero2)}")
+#
+
+# Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# def potenciaNumero (base,expoente):
+#   return base ** expoente
+# base = int(input("Digite o número da base: \n"))
+# expoente = int(input("Digite o número do expoente: \n"))
+# print(f"O número {base} elevado a potencia {expoente} é {potenciaNumero(base,expoente)}")
+
+
+# Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# def celciusParaFarenheit(celsius):
+#   return (celsius * 9/5) + 32
+# inserirTemp = int(input("Digite a temperatura em fahrenheit :"))
+# print(f"A temperatura em celsius0 é {celciusParaFarenheit(inserirTemp)}")
